9d531a3cca8c77e3e878b0f12d2b6fb58986ad5f
sadingc <103971417+sadingc@users.noreply.github.com>
2022-07-16 10:19:15 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 api_hash = "a86730aab5c59953c424abb4396d32d5"
 api_id = 14681595
-bot_token = "5232913510:AAEwPW72m2fPJ_sYhg29eDPTbnxNrvRZWi0"
-bot_admin_group = -1001689722383
+bot_token = "5232913510:AAHfXFgZb6MSNx2Tn9QTo1B9VMxT1D_RBGs"
+bot_admin_group = -1001702909339
+
