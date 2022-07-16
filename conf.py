4577af95369ad034188d905a5c13f08b04b3105f
sadingc <103971417+sadingc@users.noreply.github.com>
2022-07-16 11:19:49 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,7 @@
-api_hash = "a86730aab5c59953c424abb4396d32d5"
-api_id = 14681595
-bot_token = "5232913510:AAHfXFgZb6MSNx2Tn9QTo1B9VMxT1D_RBGs"
-bot_admin_group = -1001702909339
+api_hash = "c6ef54eb0cdd29441ae33921531cb4e8"
 
+api_id = 19919438
+
+bot_token = "5384728457:AAEAerlmbBwRWZFE8GTp9lJ-xOPJw4vhBAU"
+
+bot_admin_group = -1001702909339
